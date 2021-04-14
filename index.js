86c8d3cb033545339ba71cbb2c123a198959b4b7
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-14 14:19:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ let path = require('path')
 let fs = require('fs')
 let package = require('./package.json')
 const CFonts  = require('cfonts')
-CFonts.say('Lightweight\zhicco Bot', {
+CFonts.say('welcome\zhicco Bot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
