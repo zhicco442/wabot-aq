5c770c1ddc5c31180c1f556eede623f9457d1386
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-14 14:46:13 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,8 +1,30 @@
 let handler = async m => m.reply(`
-╭─「 Donasi • Pulsa 」
-│ • Indosat Ooredoo [081515860089]
-│ • Telkomsel [081334177340]
-╰────
+const donasi = (pushname, prefix, botName, ownerName) => { 
+	return `Hallo, ${pushname} 👋
+Mau donasi ya kak ✨
+ اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ
+_“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
+╔════════════════════
+║ *DONASI UNTUK ${*MR.CHAINNERBOT}
+╠════════════════════
+║╭──❉ *DONASI BOS* ❉─────
+║│➸ *OVO*:
+║│➸ *DANA*: _0882-8982-4963_
+║│➸ *PULSA*: _0882-8982-4963_
+║│➸ *GOPAY*: _0882-8982-4963_
+║╰──────────────────
+╠════════════════════
+║         ${ZHICCO BOT}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${ZHICCO BOT}
+╠════════════════════
+║ _*POWERED BY ${Zhicco BOT}*_
+╚════════════════════`
+}
+
+exports.donasi = donasi
+
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
 handler.tags = ['info']
