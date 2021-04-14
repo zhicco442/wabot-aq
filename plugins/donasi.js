3935e9d809055f204942b89b3d4fa8672aa38025
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-14 16:17:53 +0700

    UPDATE donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,3 +1,4 @@
+let handler = async m => m.reply(`
 Mau donasi ya kak ✨
  اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ
 _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
