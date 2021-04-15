bd6576f042b3699612b4ce02b575a09c07cf79ef
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-15 10:58:15 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,5 +1,4 @@
-const donasi = (pushname, prefix, botName, ownerName) => { 
-	return `Hallo, ${pushname} 👋
+let handler = async m => m.reply(`
 Mau donasi ya kak ✨
  اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ
 _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
@@ -7,10 +6,10 @@ _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). J
 ║ *DONASI UNTUK ${*MR.CHAINNERBOT}
 ╠════════════════════
 ║╭──❉ *DONASI BOS* ❉─────
-║│➸ *OVO*:
-║│➸ *DANA*: _0882-8982-4963_
-║│➸ *PULSA*: _0882-8982-4963_
-║│➸ *GOPAY*: _0882-8982-4963_
+║│➸ *OVO*:  _0812-1281-4187_
+║│➸ *DANA*: _0812-1281-4187_
+║│➸ *PULSA*: _0812-1281-4187_
+║│➸ *GOPAY*: _0812-1281-4187_
 ║╰──────────────────
 ╠════════════════════
 ║         ${ZHICCO BOT}
@@ -21,5 +20,10 @@ _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). J
 ║ _*POWERED BY ${Zhicco BOT}*_
 ╚════════════════════`
 }
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
 
-exports.donasi = donasi
+module.exports = handler
+
+module.exports = handler
