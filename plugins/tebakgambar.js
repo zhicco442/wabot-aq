72766be458e843c2c2ba50dabf461a88299e5a66
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-15 11:10:39 +0700

    Update tebakgambar.js

--- plugins/tebakgambar.js
+++ plugins/tebakgambar.js
@@ -32,3 +32,14 @@ Bonus: ${poin} XP
   handler.command = /^tebakgambar/i
   
   module.exports = handler
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
