ea1c3589618ccc14e484173b140ee7ad5be99fb2
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-19 20:39:15 +0700

    Create donasi.js

---
+++ plugins/donasi..js
@@ -0,0 +1,29 @@
+let handler = async m => m.reply(`
+Mau donasi ya kak ✨
+ اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ
+_“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
+╔════════════════════
+║ *DONASI UNTUK ${*MR.CHAINNERBOT}
+╠════════════════════
+║╭──❉ *DONASI BOS* ❉─────
+║│➸ *OVO*:  _0812-1281-4187_
+║│➸ *DANA*: _0812-1281-4187_
+║│➸ *PULSA*: _0812-1281-4187_
+║│➸ *GOPAY*: _0812-1281-4187_
+║╰──────────────────
+╠════════════════════
+║         ${ZHICCO BOT}
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║  ▌│█║▌║▌║║▌║▌║█│▌
+║         ${ZHICCO BOT}
+╠════════════════════
+║ _*POWERED BY ${Zhicco BOT}*_
+╚════════════════════`
+}
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
+
+module.exports = handler
