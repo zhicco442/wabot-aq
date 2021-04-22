c55de01a1d13acf890ebf26843edb89189511c3e
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-22 19:47:39 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,5 +1,5 @@
 let handler = function (m) {
-  // this.sendContact(m.chat, '6281212814187', 'zhicco', m)
+  // this.sendContact(m.chat, '+6281212814187', 'zhicco', m)
   this.sendContact(m.chat, '0', 'Insert Owner Number Here', m)
 }
 handler.help = ['owner', 'creator']
