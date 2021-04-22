5b94a54bd2f062fd9a4bfff3975eafc6d760451d
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-22 23:49:42 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,7 +3,7 @@ Mau donasi ya kak ✨
  اتَّقوا النَّارَ ولو بشقِّ تمرةٍ ، فمن لم يجِدْ فبكلمةٍ طيِّبةٍ
 _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). Jika kamu tidak punya, maka bisa dengan kalimah thayyibah” [HR. Bukhari 6539, Muslim 1016]_
 ╔════════════════════
-║ *DONASI UNTUK [BOT WA ZHICCO]
+║ *DONASI UNTUK [BOT WA ZHICCO]*
 ╠════════════════════
 ║╭──❉ *DONASI BOS* ❉─────
 ║│➸ *OVO*:  _0812-1281-4187_
@@ -12,10 +12,10 @@ _“jauhilah api neraka, walau hanya dengan bersedekah sebiji kurma (sedikit). J
 ║│➸ *GOPAY*: _0812-1281-4187_
 ║╰──────────────────
 ╠════════════════════
-║        [mr.chainner BOT]
+║       *[mr.chainner BOT]*
 ║  ▌│█║▌║▌║║▌║▌║█│▌
 ║  ▌│█║▌║▌║║▌║▌║█│▌
-║         [BOT WA ZHICCO]
+║         *[BOT WA ZHICCO]*
 ╠════════════════════
 ║ _*POWERED BY [zhicco wa BOT]*
 ╚════════════════════`
