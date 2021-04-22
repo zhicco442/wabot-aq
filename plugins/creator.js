30a15ec20a443a2a889d5b7c2ccbcce92b734cbd
zhicco442 <79734552+zhicco442@users.noreply.github.com>
2021-04-23 00:06:48 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -1,6 +1,6 @@
 let handler = function (m) {
   // this.sendContact(m.chat, '6281212814187', 'zhicco', m)
-  this.sendContact(m.chat, '0', 'Zhicco owner bot', m)
+  this.sendContact(m.chat, '+62 812-1281-4187', 'Zhicco owner bot', m)
 }
 handler.help = ['owner', 'creator']
 handler.tags = ['info']
